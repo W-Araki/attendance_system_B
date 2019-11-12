016ab391993c60f43ec597152bd11a7e200cba4e
W-Araki <aino18nichidai18@gmail.com>
2019-11-12 04:58:41 +0000

    5-5-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
+  def index
+  end
+  
   def new
+    @user = User.new
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+
+private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
