09451bbba19ef233afd798581140b4c778adbbc2
W-Araki <aino18nichidai18@gmail.com>
2019-11-19 11:04:10 +0000

    8-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+    
   def index
   end
   
@@ -9,9 +11,6 @@ class UsersController < ApplicationController
   end
   
   def show
-  end
-  
-  def edit
   end
   
   def create
@@ -25,18 +24,41 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
   def update
+     if @user.update_attributes(user_params)
+       flash[:success] = "更新しました。"
+       redirect_to user_url @user
+     else
+       render :edit
+     end
   end
   
   def destroy
   end
 
 private
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+   
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to root_url
+      end
+    end
 
-   def set_user
-    @user = User.find(params[:id])
-   end
-  
    def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
